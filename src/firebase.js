2abdbb35a407c1667eee64fe29ffcee25f0896c5
utkarshh0110 <utkarshh0110@gmail.com>
2021-06-22 11:24:26 +0530

    creating env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDCBjvVuumtDGG7RCNC6bnqqXghnTU__Nw",
-    authDomain: "clinikk-73af7.firebaseapp.com",
-    projectId: "clinikk-73af7",
-    storageBucket: "clinikk-73af7.appspot.com",
-    messagingSenderId: "763428268390",
-    appId: "1:763428268390:web:3165d7626a4c923a8f80a7"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
